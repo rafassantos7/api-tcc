dba040e45c571a6b77e7745d96cc9f84d4a28baa
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2025-11-10 12:18:00 +0000

    deixar somente a parte de front

--- api-tcc-main - 2/api-tcc-api/api-tcc-api/api/src/main/java/com/example/aula/exception/EmailJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.aula.exception;
-
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String message) {
-        super(message);
-    }
-}
--- api-tcc-main - 2/api-tcc-main/api-tcc-api/api-tcc-api/api/src/main/java/com/example/aula/repository/RotinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.aula.repository;
-
-import com.example.aula.model.Rotina;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RotinaRepository extends JpaRepository<Rotina, Long> {
-}
