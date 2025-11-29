6c600e884a7e65e55ad63830d2ef0ca848b7d6d7
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2025-11-28 21:49:20 -0300

    Delete api-tcc-api/api directory

--- api-tcc-api/api/src/main/java/com/example/levelUp/exception/EmailJaCadastradoException.java
+++
@@ -1,7 +0,0 @@
-package com.example.levelUp.exception;
-
-public class EmailJaCadastradoException extends RuntimeException {
-    public EmailJaCadastradoException(String message) {
-        super(message);
-    }
-}
