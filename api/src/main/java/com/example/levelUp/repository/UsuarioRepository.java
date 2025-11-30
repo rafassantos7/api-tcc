c8c71b6c732c260d498c1c36b4567c51778ef0da
Rafael Vitor <160610194+rafassantos7@users.noreply.github.com>
2025-11-30 11:26:55 -0300

    Change findByEmail to return Optional<Usuario>

--- api/src/main/java/com/example/levelUp/repository/UsuarioRepository.java
+++ api/src/main/java/com/example/levelUp/repository/UsuarioRepository.java
@@ -1,16 +1,17 @@
 package com.example.levelUp.repository;
 
+import java.util.Optional;
 import com.example.levelUp.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     // Para Spring Security
-    UserDetails findByEmail(String email);
+    Optional<Usuario> findByEmail(String email);
 
     // Para suas operações de negócio
     boolean existsByEmail(String email);
+
 }
